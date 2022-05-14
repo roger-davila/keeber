3512f66d92fb3f3b6f3e0a6c12823099373c2909
Roger Davila <rogerddavila@gmail.com>
2022-05-14 13:46:53 -0700

    add likes field with m2m relationship

--- main_app/models.py
+++ main_app/models.py
@@ -3,18 +3,20 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 
 SIZES = (
-  ('full sized', 'Full Sized'),
-  ('1800 compact', '1800 Compact'),
-  ('tenkeyless', 'Tenkeyless'),
-  ('75%', '75%'),
-  ('65%', '65%'),
-  ('60%', '60%'),
-  ('40%', '40%'),
-  ('number pad', 'Number Pad'),
-  ('Macro pad', 'Macro Pad'),
+    ('full sized', 'Full Sized'),
+    ('1800 compact', '1800 Compact'),
+    ('tenkeyless', 'Tenkeyless'),
+    ('75%', '75%'),
+    ('65%', '65%'),
+    ('60%', '60%'),
+    ('40%', '40%'),
+    ('number pad', 'Number Pad'),
+    ('Macro pad', 'Macro Pad'),
 )
 
 # Create your models here.
+
+
 class Keyboard(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     case = models.CharField(max_length=100)
@@ -24,9 +26,11 @@ class Keyboard(models.Model):
     plate = models.CharField(max_length=100, blank=True, null=True)
     stabilizers = models.CharField(max_length=100, blank=True, null=True)
     split = models.BooleanField(default=False)
+    likes = models.ManyToManyField(User, related_name='keyboard_post')
+    time_created = models.DateTimeField(auto_now_add=True)                                   gttz
 
     def __str__(self):
         return f'{self.case} with {self.switches} switches and {self.keycaps} keycaps'
 
     def get_absolute_url(self):
-      return reverse('detail', kwargs={'keyboard_id': self.id})
+        return reverse('detail', kwargs={'keyboard_id': self.id})

38dc109cb32c24e20535b69c2c82f663611d5ef8
Roger Davila <rogerddavila@gmail.com>
2022-05-14 16:22:16 -0700

    add signup path

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('keyboards/', views.keyboards_index, name='index'),
+  path('accounts/signup/', views.signup, name='signup'),
 ]

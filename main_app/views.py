1c5aaf771ff5dacaff4982b64bf6d199fb254736
Roger Davila <rogerddavila@gmail.com>
2022-05-14 16:23:04 -0700

    add signup view method to render signup page and create new users

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import Keyboard
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')  
+    return render(request, 'home.html')
 
 def keyboards_index(request):
     keyboards = Keyboard.objects.all()
     return render(request, 'keyboards/index.html', {'keyboards': keyboards})
+
+def keyboard_detail(request, keyboard_id):
+    keyboard = Keyboard.objects.get(id=keyboard_id)
+    return render('keyboards/detail.html', {'keyboard': keyboard})
+
+def signup(request):
+    error_message = ''
+    # Code block below only executes if the from is submitted
+    if request.method == 'POST':
+        print(request.POST)
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user) #Logs user in after saving to user table
+            return redirect('keyboards')
+        else:
+            error_message = 'Invalid sign up - try again'
+    # This is the GET method of the form. Just renders a blank form
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html',context)

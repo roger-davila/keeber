b1061c49a9349f369e6bcbf900cd0132af70b382
Roger Davila <rogerddavila@gmail.com>
2022-05-15 14:11:21 -0700

    add redirects after logging in and logging out

--- keyboardcollector/settings.py
+++ keyboardcollector/settings.py
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redirect Links
+LOGIN_REDIRECT_URL = '/keyboards/'
+LOGOUT_REDIRECT_URL = '/keyboards/'

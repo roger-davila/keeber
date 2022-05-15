016c7c0f97a35f245c92b24d351d92ae66eb30c3
Roger Davila <rogerddavila@gmail.com>
2022-05-15 14:16:02 -0700

    add login and user profile paths

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView
+from main_app.forms import UserLoginForm
 from . import views
+
 
 urlpatterns = [
   path('', views.home, name='home'),
   path('keyboards/', views.keyboards_index, name='index'),
   path('accounts/signup/', views.signup, name='signup'),
+  path(
+    'accounts/login/', 
+    LoginView.as_view(authentication_form = UserLoginForm),
+    name="login"
+  ),
+  path('user/<int:user_id>/', views.user_profile, name='profile'),
+  path('user/<int:user_id>/keyboards/', views.user_keyboards, name='keyboards'),
 ]

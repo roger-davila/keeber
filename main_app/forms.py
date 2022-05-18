0189e2496abf1d326adde3d8b3eef6359639461c
Roger Davila <rogerddavila@gmail.com>
2022-05-17 23:40:45 -0700

    udpate widgets for user registration form

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,8 +1,16 @@
 
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
 
 class UserLoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs.update({'class': 'input'})
-        self.fields['password'].widget.attrs.update({'class': 'input'})
+        for visible in self:
+            visible.field.widget.attrs.update({'class': 'input'})
+
+
+class UserRegistrationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for visible in self:
+            visible.field.widget.attrs.update({'class': 'input'})
